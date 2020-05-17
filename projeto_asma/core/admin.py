5ba447ccd742533e32aa913198fb31efb789b432
Arthur Almeida <arthurdralmeida@bitbucket.com>
2020-05-17 18:40:08 -0300

    objects to admin permissions

--- projeto_asma/core/admin.py
+++ projeto_asma/core/admin.py
@@ -3,3 +3,11 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Atendente)
+admin.site.register(Paciente)
+admin.site.register(Sintoma)
+admin.site.register(AgenteDeSaude)
+admin.site.register(OrientacoesMedicas)
+admin.site.register(DiarioDeSintomas)
+admin.site.register(NotificacaoDeAtividade)
+admin.site.register(Atividade)
+admin.site.register(DadosFitBit)
